8eb778406206b20b57a9e9b0db8a2573025c59e0
devREMY1 <dev.maksyuta@gmail.com>
2024-12-01 20:29:51 +0200

    add navigation and logo

---
+++ app/js/script.js
@@ -0,0 +1,50 @@
+let deferredPrompt;
+
+// Слушаем событие `beforeinstallprompt`
+window.addEventListener('beforeinstallprompt', e => {
+	// Останавливаем автоматическое отображение
+	e.preventDefault();
+	deferredPrompt = e;
+	document.getElementById('installApp').style.display = 'block';
+});
+
+// Обрабатываем нажатие на кнопку
+document.getElementById('installApp').addEventListener('click', () => {
+	if (deferredPrompt) {
+		deferredPrompt.prompt();
+		deferredPrompt.userChoice.then(choiceResult => {
+			if (choiceResult.outcome === 'accepted') {
+				console.log('Застосунок встановлено');
+			} else {
+				console.log('Встановлення застосунку відмінено');
+			}
+			deferredPrompt = null;
+		});
+	}
+});
+
+// Регистрация service-worker
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('./service-worker.js')
+		.then(registration => {
+			console.log('Service Worker зарегистрирован: ', registration);
+		})
+		.catch(error => {
+			console.error('Ошибка регистрации Service Worker: ', error);
+		});
+}
+
+// Проверяем устройство и показываем кнопку для Android
+if (window.matchMedia('(display-mode: standalone)').matches) {
+	// Приложение уже установлено
+	document.getElementById('installApp').style.display = 'none';
+} else if (/Mobi|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+	// Для мобильных устройств показываем инструкцию
+	document.body.innerHTML += `
+      <p>Для встановлення додатку на iPhone! натисніть на іконку "поділитися" в вашому браузері та натисніть "Додати на головний екран".</p>
+    `;
+} else {
+	// Для ПК показываем только сообщение
+	document.body.innerHTML = '<h1>Доступно тільки для мобільних пристроїв</h1>';
+}
